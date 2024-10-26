99a4be9521a3f964c9deb61f267c043c27dc2dc5
shaps987 <davidmshaps@gmail.com>
2024-10-26 16:46:19 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,26 +1,28 @@
-scene.onOverlapTile(SpriteKind.Player, sprites.castle.saplingOak, function (sprite, location) {
+scene.onOverlapTile(SpriteKind.Player, sprites.castle.saplingOak, function on_overlap_tile(sprite: Sprite, location: tiles.Location) {
     game.over(true)
 })
 let mySprite = sprites.create(img`
-    . . . . . . e e c c e e . . . . 
-    . . . . . e 2 2 2 2 2 2 e . . . 
-    . . . . 2 c 2 2 2 2 2 2 c 2 . . 
-    . . . e 2 c 4 2 2 2 2 2 c 2 e . 
-    . . . f 2 2 4 2 2 2 2 2 c 2 f . 
-    . . . f 2 2 4 2 2 2 2 2 2 2 f . 
-    . . . f 2 2 4 2 2 2 2 2 2 2 f . 
-    . . . f 2 c 2 4 4 2 2 2 c 2 f . 
-    . . . e 2 c e c c c c e c 2 e . 
-    . . . e 2 e c b b b b c e 2 e . 
-    . . . e 2 e b b b b b b e 2 e . 
-    . . . e e e e e e e e e e e e . 
-    . . . f e d e e e e e e d e f . 
-    . . . f e 2 d e e e e d 2 e f . 
-    . . . f f e e e e e e e e f f . 
-    . . . . f f . . . . . . f f . . 
+        . . . . . . e e c c e e . . . . 
+            . . . . . e 2 2 2 2 2 2 e . . . 
+            . . . . 2 c 2 2 2 2 2 2 c 2 . . 
+            . . . e 2 c 4 2 2 2 2 2 c 2 e . 
+            . . . f 2 2 4 2 2 2 2 2 c 2 f . 
+            . . . f 2 2 4 2 2 2 2 2 2 2 f . 
+            . . . f 2 2 4 2 2 2 2 2 2 2 f . 
+            . . . f 2 c 2 4 4 2 2 2 c 2 f . 
+            . . . e 2 c e c c c c e c 2 e . 
+            . . . e 2 e c b b b b c e 2 e . 
+            . . . e 2 e b b b b b b e 2 e . 
+            . . . e e e e e e e e e e e e . 
+            . . . f e d e e e e e e d e f . 
+            . . . f e 2 d e e e e d 2 e f . 
+            . . . f f e e e e e e e e f f . 
+            . . . . f f . . . . . . f f . .
     `, SpriteKind.Player)
 controller.moveSprite(mySprite)
-tiles.setTilemap(tilemap`level0`)
+tiles.setTilemap(tilemap`
+    level0
+`)
 tiles.placeOnRandomTile(mySprite, sprites.builtin.crowd9)
 scene.cameraFollowSprite(mySprite)
 info.startCountdown(60)
